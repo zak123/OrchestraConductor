7d1278e234acffb3102d2d56dd26a2fa96c09497
zak123 <zacharym123@gmail.com>
2015-03-26 21:08:46 -0700

    Initial Commit

---
+++ OrchestraConductor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OrchestraConductor
+//
+//  Created by Zachary Mallicoat on 3/26/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
